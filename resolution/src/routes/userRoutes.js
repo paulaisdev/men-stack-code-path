c2692c94e7298d14a4bb2222e7e4564b4bb1915c
paulaallemand <paulaisdev@gmail.com>
2023-05-23 17:48:28 -0300

    removed unused code

--- resolution/src/routes/userRoutes.js
+++ resolution/src/routes/userRoutes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 import controller from "../controllers/userController";
 import authController from "../controllers/authController";
 
-router.get("/all", controller.getAll);
-router.post("/create", controller.createUser);
+router.get("/", controller.getAll);
+router.post("/", controller.createUser);
 router.post("/login", authController.login);
-router.patch("/update/:id", controller.updateUserById);
-router.delete("/delete/:id", controller.deleteUserById);
+router.patch("/:id", controller.updateUserById);
+router.delete("/:id", controller.deleteUserById);
 
 export default router;
