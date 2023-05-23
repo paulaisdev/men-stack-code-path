9d33f2b9185588b039b3656b563ca9c9ce7dbd4f
paulaallemand <paulaisdev@gmail.com>
2023-05-23 17:03:45 -0300

    added message at the first controller

--- class/src/controllers/moviesControllers.js
+++ class/src/controllers/moviesControllers.js
@@ -1,6 +1,11 @@
 import movies from "../models/movieSchema";
 
-const home = (req, res) => {};
+const home = (req, res) => {
+  res.status(200).send({
+    statusCode: 200,
+    message: "Welcome to Teraflix!",
+  });
+};
 
 // DEMANDA: retornar todos os filmes
 
